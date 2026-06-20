45433af5eb7a0fe36e649c9ebcafbbbd544a969b
revathy82 <revathyramsa@gmail.com>
2026-06-20 23:08:02 +0000

    Arya week 2 work completed.

--- lesson2/classwork/practice.py
+++ lesson2/classwork/practice.py
@@ -1,25 +1,39 @@
 # Problem 1
 # Ask the user for their age.
 # Calculate and print how many decades old they are, rounded to the nearest whole number.
+age_input=input("Enter your age: 11")
+age = int(age_input)
+decades=round(age/10)
+print("you are approximately",decades,"decades old")
+
 
 
 
 # Problem 2
 # Ask the user to enter a number.
 # Print the result of multiplying that number by 5.
-
+num_input=input("100")
+number=float(num_input)
+result=number*5
+print("The result is:",result)
 
 
 # Problem 3
 # Use a for loop to print "I will learn Python!" 3 times.
-
+for i in range(3):
+    print("I will learn python!")
 
 
 # Problem 4
 # Ask the user for their name and age.
 # Print their name and how old they will be one year in a single sentence.
-
-
+name = input("enter your name: arya")
+age = int(input("enter your age:11"))
+# calculate age next year11
+next_age=age+1
+print (f"HELLO {name}, next year you will be {next_age} years old")
 
 # Problem 5
 # Use a for loop to print the numbers from 2 to 8, one per line.
+for i in range (5,9):
+    print(i)
--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -1,26 +1,43 @@
 # Homework Problem 1
 # Ask the user for two numbers.
 # Print their quotient and remainder on separate lines.
-
+number1=int(input("enter first number: "))
+number2=int(input("enter second number: "))
+quotient=number1/number2
+remainder=number1%number2
+print("Remainder is ", remainder)
+print(f"Quotient is {quotient}")
 
 
 # Homework Problem 2
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
-
+animal=input("favorite animal")
+color=input("favorite color")
+print(f"A {color} {animal} would be awesome!")
 
 
 # Homework Problem 3
 # Use a for loop to print all the even numbers from 0 to 10 (including 10).
+for i in range(0,11,2):
+    print(i)
 
-
+for i in range(0,12):
+    if(i%2 == 0):
+        print(i)
 
 # Homework Problem 4
 # Ask the user how many push-ups they can do.
 # Multiply it by 7 and print how many they could do in a week.
+pushupcount=int(input("how many push-ups you can do?"))
+weeklycount=pushupcount * 7
+print(weeklycount)
 
 
 
 # Homework Problem 5
 # Use a for loop to print the square of each number from 1 to 6.
 # (Example: 1*1=1, 2*2=4, etc.)
+
+for i in range(1,7):
+    print(i*i)
